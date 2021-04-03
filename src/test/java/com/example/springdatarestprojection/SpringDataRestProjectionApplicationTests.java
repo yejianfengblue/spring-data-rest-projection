408fd27819477f8e1d6e3eea6c389714328ca365
Jian Feng Ye <yejianfengblue@163.com>
2021-04-03 15:14:30 +0800

    Test create person

--- src/test/java/com/example/springdatarestprojection/SpringDataRestProjectionApplicationTests.java
+++ src/test/java/com/example/springdatarestprojection/SpringDataRestProjectionApplicationTests.java
@@ -1,14 +1,19 @@
 package com.example.springdatarestprojection;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.HashMap;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,6 +25,9 @@ class SpringDataRestProjectionApplicationTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     @SneakyThrows
     void getAllPersons() {
@@ -30,6 +38,24 @@ class SpringDataRestProjectionApplicationTests {
                .andExpect(jsonPath("_embedded.persons[0].name").value("Alice"))
                .andExpect(jsonPath("_embedded.persons[0].address.street").value("Secret Street"))
                .andExpect(status().isOk())
+               .andDo(print());
+    }
+
+    @Test
+    @SneakyThrows
+    void createPerson() {
+
+        HashMap<String, Object> addressJson = new HashMap<>();
+        addressJson.put("street", "T Street");
+
+        HashMap<String, Object> personJson = new HashMap<>();
+        personJson.put("name", "Tim");
+        personJson.put("address", addressJson);
+
+        mockMvc.perform(post("/persons")
+                                .content(objectMapper.writeValueAsString(personJson))
+                                .contentType(MediaType.APPLICATION_JSON))
+               .andExpect(status().isCreated())
                .andDo(print());
     }
 
